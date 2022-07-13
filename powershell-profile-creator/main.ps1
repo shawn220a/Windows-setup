549dc31e690edcf341a4d7d98eb907fcc4e4e3b5
shawn220a <shawn.42496@gmail.com>
2022-07-13 05:51:17 -0500

    Add first function to ps1

--- powershell-profile-creator/main.ps1
+++ powershell-profile-creator/main.ps1
@@ -1,1 +1,11 @@
-Write-Output 'Hello'
+# New-Item -ItemType File $profile
+
+$path = 'C:\Users\shawn\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
+
+@'
+function gs {
+  git status -s
+}
+'@ > $path
+
+notepad $path 
